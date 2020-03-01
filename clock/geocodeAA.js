2e6a67d8504bbef16ee3ee064d1783ac0e453540
alvinashiatey <alvinashiatey@gmail.com>
2020-03-01 12:49:05 -0500

    changes

--- clock/geocodeAA.js
+++ clock/geocodeAA.js
@@ -1,45 +1,51 @@
 let googleAPI;
 googleAPI = "AIzaSyDKdwsFsIw1STe6Xf2zUFH0Sk8n6YQza40";
-window.addEventListener("load", geocode());
 
-function geocode() {
-  navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    // const weatherAPI = ``;
+// displaying received datat on UI
 
-    axios
-      .get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {
-          latlng: `${lat},${long}`,
-          key: googleAPI
-        }
-      })
-      .then(response => {
-        // log full response
-        //console.log(response);
+//geolocating
+var geoLocate = (function() {
+  navigator.geolocation.getCurrentPosition(getLocation);
 
-        //location
+  function getLocation(position) {
+    var geoLo = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    };
+    googleMapRequest(geoLo.lat, geoLo.long);
+    darkSky(geoLo.lat, geoLo.long);
+  }
+
+
+  // fetching data from Api
+  function googleMapRequest(lat, lng) {
+    var googleAPI = "AIzaSyDKdwsFsIw1STe6Xf2zUFH0Sk8n6YQza40";
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        latlng: `${lat},${lng}`,
+        key: googleAPI
+      }
+    })
+    .then(response => {
+        console.log(response);
         let location1 = response.data.results[7].formatted_address;
         let pinLocation1 = document.querySelector("#location1");
 
         pinLocation1.textContent += location1.padStart();
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.log(error);
       });
+  }
 
+  function darkSky(lat, lng){
     axios
       .get(
-        `https://api.darksky.net/forecast/b20a7ac99ea1d93df5831a1541ba3cb6/${lat},${long}`
+        `https://api.darksky.net/forecast/b20a7ac99ea1d93df5831a1541ba3cb6/${lat},${lng}`
       )
-      .then(response => {
-        //log response
-        //console.log(response);
-
-        //temperature and summary
-        let weather1 = document.querySelector("#weather1");
+      .then(response => {  
+          console.log(response);
+          let weather1 = document.querySelector("#weather1");
         const { temperature, summary } = response.data.currently;
 
         let celsius = Math.floor(((temperature - 32) * 5) / 9);
@@ -49,9 +55,13 @@ function geocode() {
       .catch(error => {
         console.log(error);
       });
-  });
-}
+  }
 
+})();
+
+
+
+/// fine tune into modules 
 (function geocode2() {
   let address;
   const getAddress = document.querySelector("#submit");
--- clock/script.js
+++ clock/script.js
@@ -1,34 +1,49 @@
-// fetching data from Api
 // displaying received datat on UI
 
-//geolocating 
+//geolocating
+var geoLocate = (function() {
+  navigator.geolocation.getCurrentPosition(getLocation);
 
-var geoLocate = (function (){
-
-    var gAPI ="AIzaSyDKdwsFsIw1STe6Xf2zUFH0Sk8n6YQza40";
-
-
-
-    navigator.geolocation.getCurrentPosition(position =>{
+  function getLocation(position) {
     var geoLo = {
-            lat : position.coords.latitude,
-            long : position.coords.longitude
-        }
-        return geoLo
-                        
-        })
-  
-        console.log(geoLo);
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    };
+    googleMapRequest(geoLo.lat, geoLo.long);
+    darkSky(geoLo.lat, geoLo.long);
+  }
 
-    
+
+  // fetching data from Api
+  function googleMapRequest(lat, lng) {
+    var googleAPI = "AIzaSyDKdwsFsIw1STe6Xf2zUFH0Sk8n6YQza40";
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        latlng: `${lat},${lng}`,
+        key: googleAPI
+      }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+      });
+  }
+
+  function darkSky(lat, lng){
+    axios
+      .get(
+        `https://api.darksky.net/forecast/b20a7ac99ea1d93df5831a1541ba3cb6/${lat},${lng}`
+      )
+      .then(response => {  
+          console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
 })();
 
-
-var Pol = (function(Gel){
-
-var Pal = Gel;
-console.log(Pal)
-    
-})(geoLocate);
 
