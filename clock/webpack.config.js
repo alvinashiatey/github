3af57ae52ad0259072a2bd08d9c8a5771b1ca919
alvinashiatey <alvinashiatey@gmail.com>
2020-03-10 16:08:43 -0400

    babel

--- clock/webpack.config.js
+++ clock/webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
     })
   ],
   module: {
-    rules: [{}]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
